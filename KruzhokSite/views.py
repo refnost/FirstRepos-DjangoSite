be143cd9cccc2be7921957afae77fbc0616fc1f4
Refnost <stepanrefn@gmail.com>
2020-12-16 19:48:23 +0300

    some updates

--- KruzhokSite/views.py
+++ KruzhokSite/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def profile(request):
-    return render(request, 'profile/index.html', {})
+    posts = Post.objects.all()
+    return render(request, 'profile/index.html', {'posts': posts})
